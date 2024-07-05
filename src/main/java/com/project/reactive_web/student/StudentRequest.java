2f906e7e30aaa1b46c71654abd3bf1122dd3c51a
RahulGupta <rahulrgd21@gmail.com>
2024-07-05 17:21:39 +0530

    Created dto StudentRequest

---
+++ src/main/java/com/project/reactive_web/student/StudentRequest.java
@@ -0,0 +1,13 @@
+package com.project.reactive_web.student;
+
+
+import lombok.Data;
+
+@Data
+public class StudentRequest {
+
+  private String firstname;
+  private String lastname;
+  private int age;
+
+}
